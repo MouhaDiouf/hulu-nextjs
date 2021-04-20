c00121ef84241a7aa571185eb6bc776370b847dd
Mouha <mouhamadoudioufsn@gmail.com>
2021-04-20 23:48:34 +0000

    Add hulu  image to the header

--- components/Header.js
+++ components/Header.js
@@ -1,5 +1,11 @@
+import Image from "next/image";
 function Header() {
-  return <header></header>;
+  return (
+    <header className="header">
+      <h1>This is the header</h1>
+      <Image src="https://links.papareact.com/us6" width={200} height={100} />
+    </header>
+  );
 }
 
 export default Header;
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Header from "../components/Header";
 
 export default function Home() {
   return (
@@ -7,7 +8,10 @@ export default function Home() {
         <title>Hulu 2.0</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <h1>Let's built hulu 2.0</h1>
+      {/* Header */}
+      <Header />
+      {/* Nav */}
+      {/* Results */}
     </div>
   );
 }
