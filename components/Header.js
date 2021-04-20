252ff4285ab570872fdc6e0a7d0dab0b9283c254
Mouha <mouhamadoudioufsn@gmail.com>
2021-04-20 23:53:16 +0000

    Change background color to dark

--- components/Header.js
+++ components/Header.js
@@ -3,7 +3,12 @@ function Header() {
   return (
     <header className="header">
       <h1>This is the header</h1>
-      <Image src="https://links.papareact.com/us6" width={200} height={100} />
+      <Image
+        src="https://links.papareact.com/us6"
+        width={200}
+        height={100}
+        className="object-contain"
+      />
     </header>
   );
 }
