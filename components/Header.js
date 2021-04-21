e0ba42be9d6d94d13b5694ab22ce08d8458754b0
Mouha <mouhamadoudioufsn@gmail.com>
2021-04-21 14:28:27 +0000

    Add styling to the header

--- components/Header.js
+++ components/Header.js
@@ -10,8 +10,8 @@ import {
 } from "@heroicons/react/outline";
 function Header() {
   return (
-    <header className="header">
-      <div>
+    <header className="flex flex-col sm:flex-row m-5 justify-between items-center h-auto">
+      <div className="flex flex-grow justify-evenly max-w-2xl">
         <HeaderItem title="HOME" Icon={HomeIcon} />
         <HeaderItem title="TRENDING" Icon={LightningBoltIcon} />
         <HeaderItem title="VERIFIED" Icon={BadgeCheckIcon} />
